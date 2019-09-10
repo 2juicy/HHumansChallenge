5f9aa75a3d07faebd95b7197cdce41a92ffa329e
JohnnyC <j0hnnywchiang@gmail.com>
2019-09-10 15:37:05 -0700

    header bar completed so far

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header id="header-component">
+      <div className="logo">
+        <a href="#0">
+          <img
+            alt="Helpful Humans"
+            src={process.env.PUBLIC_URL + "assets/logo.svg"}
+          />
+        </a>
+      </div>
+      <input className="search" label="Search" placeholder="Search" />
+    </header>
+  );
+}
