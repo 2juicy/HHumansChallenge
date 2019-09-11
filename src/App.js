212922b317d46215b53cd53c7a0bc8c033c24392
JohnnyC <j0hnnywchiang@gmail.com>
2019-09-11 15:01:16 -0700

    reorganize to app level

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import List from "./components/List/List";
+import Pagination from "./components/Pagination/Pagination";
 import Detail from "./components/Detail/Detail";
+// Without a backend we can import the colors directly
+// import colors from "./colors.json";
 
 export default function App() {
+  const [colors, setColors] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const colorsPerPage = 12;
+
+  // For now we fetch locally for colors, when an API endpoint is created we can fetch from server.
+  useEffect(() => {
+    (async function() {
+      setLoading(true);
+      let res = await fetch("colors.json");
+      if (!res.ok) throw new Error("Failed to fetch");
+      let json = await res.json();
+      setColors(json);
+      setLoading(false);
+    })().catch(err => console.error(err));
+  }, []);
+
+  const indexOfLast = currentPage * colorsPerPage;
+  const indexOfFirst = indexOfLast - colorsPerPage;
+  const currentColors = colors.slice(indexOfFirst, indexOfLast);
+
+  const paginate = page => setCurrentPage(page);
+
   return (
     <div>
       <Header />
       <Sidebar />
-      <List />
+      <List colors={currentColors} loading={loading} />
+      <Pagination
+        colorsPerPage={colorsPerPage}
+        totalColors={colors.length}
+        paginate={paginate}
+      />
 
       {/* <Detail /> */}
     </div>
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,50 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Card from "../Card/Card";
-// Without a backend we can import the colors directly
-// import colors from "./colors.json";
-import Pagination from "../Pagination/Pagination";
+
 import "./List.css";
 
-export default function List() {
-  const [colors, setColors] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const colorsPerPage = 12;
-
-  // For now we fetch locally for colors, when an API endpoint is created we can fetch from server.
-  useEffect(() => {
-    (async function() {
-      setLoading(true);
-      let res = await fetch("colors.json");
-      if (!res.ok) throw new Error("Failed to fetch");
-      let json = await res.json();
-      setColors(json);
-      setLoading(false);
-    })().catch(err => console.error(err));
-  }, []);
-
-  const indexOfLast = currentPage * colorsPerPage;
-  const indexOfFirst = indexOfLast - colorsPerPage;
-  const currentColors = colors.slice(indexOfFirst, indexOfLast);
-
-  const paginate = page => setCurrentPage(page);
-
+export default function List({ colors, loading }) {
   if (loading) {
     return <h1 style={{ textAlign: "center" }}>Loading...</h1>;
   }
 
   return (
-    <React.Fragment>
-      <section id="list-section">
-        {currentColors.map(color => (
-          <Card key={color.colorId} hexString={color.hexString} />
-        ))}
-      </section>
-      <Pagination
-        colorsPerPage={colorsPerPage}
-        totalColors={colors.length}
-        paginate={paginate}
-      />
-    </React.Fragment>
+    <section id="list-section">
+      {colors.map(color => (
+        <Card key={color.colorId} hexString={color.hexString} />
+      ))}
+    </section>
   );
 }
