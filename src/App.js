7496034cddd5cab20b072d2f96c53a811acceae5
JohnnyC <j0hnnywchiang@gmail.com>
2019-09-10 19:59:54 -0700

    list component initial setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
+import List from "./components/List/List";
 
 export default function App() {
   return (
     <div>
       <Header />
       <Sidebar />
-      <h1> more stuff</h1>
+      <List />
     </div>
   );
 }
---
+++ src/components/List/List.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./List.css";
+
+export default function List() {
+  return (
+    <section id="list-section">
+      <h1>Stuff here</h1>
+    </section>
+  );
+}
