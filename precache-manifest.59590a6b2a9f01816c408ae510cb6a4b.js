d72d46e6661b7649d6ceb03440c2fbfe3a96dd51
JohnnyC <j0hnnywchiang@gmail.com>
2019-09-11 16:17:26 -0700

    Updates

---
+++ precache-manifest.59590a6b2a9f01816c408ae510cb6a4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2a04fa56ca04bfcb2a9f6ada24939b",
+    "url": "/HHumansChallenge/index.html"
+  },
+  {
+    "revision": "8d4bada6253b56e47fba",
+    "url": "/HHumansChallenge/static/css/main.9efc2cd0.chunk.css"
+  },
+  {
+    "revision": "84f74f9d120b54e04d50",
+    "url": "/HHumansChallenge/static/js/2.f5832fbc.chunk.js"
+  },
+  {
+    "revision": "8d4bada6253b56e47fba",
+    "url": "/HHumansChallenge/static/js/main.9765b608.chunk.js"
+  },
+  {
+    "revision": "ca368bd6a27ddae970b8",
+    "url": "/HHumansChallenge/static/js/runtime~main.b96de939.js"
+  }
+]);
