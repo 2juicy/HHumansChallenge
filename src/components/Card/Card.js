6426caa7905fb7c90e95088c06b94ab7bffae5df
JohnnyC <j0hnnywchiang@gmail.com>
2019-09-10 21:11:36 -0700

    card grid created

---
+++ src/components/Card/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Card.css";
+
+export default function Card() {
+  return (
+    <div className="card">
+      <div className="card-color"></div>
+      <div className="card-text">
+        <p>#cffff1</p>
+      </div>
+    </div>
+  );
+}
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,10 +1,24 @@
 import React from "react";
+import Card from "../Card/Card";
 import "./List.css";
 
 export default function List() {
   return (
     <section id="list-section">
-      <h1>Stuff here</h1>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </section>
   );
 }
