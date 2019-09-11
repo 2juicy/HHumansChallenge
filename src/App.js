a795fbd2bd10e96d3530cd72bd6ba71fcd742930
JohnnyC <j0hnnywchiang@gmail.com>
2019-09-11 01:13:10 -0700

    clean up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import List from "./components/List/List";
