01adbcf585c39a23eef3ed6102d776eb5db16504
JohnnyC <j0hnnywchiang@gmail.com>
2019-10-01 15:50:56 -0700

    fix link redirect

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,7 +14,7 @@ export default function Header({ searchColor, invalid, filterInput }) {
   return (
     <header id="header-component">
       <div className="logo">
-        <a href="/">
+        <a href="#0">
           <img
             alt="Helpful Humans"
             src={process.env.PUBLIC_URL + "/assets/logo.svg"}
