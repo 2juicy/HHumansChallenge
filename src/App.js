8499c227a5d45bfb19cf3f5795aeabd1c51dc768
JohnnyC <j0hnnywchiang@gmail.com>
2019-10-01 10:45:36 -0700

    new grouped colors .json

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default function App() {
   useEffect(() => {
     (async function() {
       setLoading(true);
-      let res = await fetch("colors.json");
+      let res = await fetch("colors2.json");
       if (!res.ok) throw new Error("Failed to fetch");
       let json = await res.json();
       setColors(json);
