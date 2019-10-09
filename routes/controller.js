6203c9fe8df2ea79be1e58919e93587c16ca29df
JohnnyC <j0hnnywchiang@gmail.com>
2019-10-09 16:08:18 -0700

    setting up mongoDB import

--- routes/controller.js
+++ routes/controller.js
@@ -1,6 +1,7 @@
 const colors = require("./colors.json");
 
 module.exports = app => {
+  // Import colors.json into mongoDB to use database
   //   const db = require("../models");
 
   app.get("/api/colors", (req, res) => {
