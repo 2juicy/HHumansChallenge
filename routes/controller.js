692cb01cf0c54148ca6b54bbc383b592566b41b4
JohnnyC <j0hnnywchiang@gmail.com>
2019-10-09 16:27:36 -0700

    DB setup complete, seed data created

--- routes/controller.js
+++ routes/controller.js
@@ -4,6 +4,14 @@ module.exports = app => {
   // Import colors.json into mongoDB to use database
   //   const db = require("../models");
 
+  // app.get("/api/colors", (req, res) => {
+  //   db.Note.find({})
+  //     .sort({ _id: -1 })
+  //     .then(function(results) {
+  //       res.json(results);
+  //     });
+  // });
+
   app.get("/api/colors", (req, res) => {
     res.json(colors);
   });
