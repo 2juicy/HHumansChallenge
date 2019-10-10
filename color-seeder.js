d2971d63b7742e8e4d09e31e2817e7741c5ed7af
JohnnyC <j0hnnywchiang@gmail.com>
2019-10-09 20:57:21 -0700

    moved seed to main dir

---
+++ color-seeder.js
@@ -0,0 +1,23 @@
+// Navigate to the /HHumansChallenge/model folder and run "node color-seeder.js" to add the seed into database
+const seed = require("../model/seed");
+const Color = require("./model/Color");
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/mongoColors");
+
+const colors = [];
+
+seed.forEach(color => {
+  colors.push(new Color(color));
+});
+
+let done = 0;
+
+for (let i in colors) {
+  colors[i].save(() => {
+    done++;
+    if (done === colors.length) {
+      mongoose.disconnect();
+    }
+  });
+}
